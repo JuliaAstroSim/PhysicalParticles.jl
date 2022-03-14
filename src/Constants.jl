38d86bacbb00cdafd4615f7ef90c7b45fe1751ff
islent <leoislent@gmail.com>
2022-03-14 16:47:57 +0800

    add constants

--- src/Constants.jl
+++ src/Constants.jl
@@ -4,17 +4,18 @@
 #@constant(MONDianGravitationalConstant, ACC0, "Modified gravitational acceleration", 1.2e-8, BigFloat(1.2e-8),
 #            u"cm/s^2", 0.0, BigFloat(0.0), "Milgrom 1983")
 const ACC0 = 1.2e-8u"cm/s^2"
+const HubbleConstant = 74.03u"km/s/Mpc"
 
-struct Constant{CC, GC, ACC0, MASS, K_B}
+struct Constant{CC, GC, Planck, EC, ACC0, MASS, SB, HC, K_B}
     c::CC     # light speed
     G::GC     # Newtonian constant of gravitation
-    #h::AbstractQuantity     # Planck constant
-    #e::AbstractQuantity     # Elementary charge
+    h::Planck     # Planck constant
+    e::EC     # Elementary charge
     m_e::MASS   # Electron mass
     m_n::MASS   # Neutron mass
     m_p::MASS   # Protron mass
-    #σ::AbstractQuantity     # Stefan-Boltzmann constant
-    #H::HC     # Hubble constant
+    σ::SB     # Stefan-Boltzmann constant
+    H::HC     # Hubble constant
     k_B::K_B    # Kelvin-Boltzmann constant
 
     ACC0::ACC0  # Modified gravitational acceleration constant
@@ -48,13 +49,13 @@ ustrip(Constant())
 function Constant(units = uAstro;
     c_0 = CODATA2018.c_0,
     G = CODATA2018.G,
-    #h = CODATA2018.h,
-    #e = CODATA2018.e,
+    h = CODATA2018.h,
+    e = CODATA2018.e,
     m_e = CODATA2018.m_e,
     m_n = CODATA2018.m_n,
     m_p = CODATA2018.m_p,
-    #σ = CODATA2018.σ,
-    #H = H,
+    σ = CODATA2018.σ,
+    H = HubbleConstant,
     k_B = CODATA2018.k_B,
     ACC0 = ACC0,
 )
@@ -69,13 +70,13 @@ function Constant(units = uAstro;
     return Constant(
         uconvert(getuVel(units), c_0),
         uconvert(units[1]^3 / units[6] / units[2]^2, G),
-        #uconvert(units[6] * units[1]^2 / units[2], h),
-        #uconvert(units[3] * units[2], e),
+        uconvert(units[6] * units[1]^2 / units[2], h),
+        uconvert(units[3] * units[2], e),
         uconvert(getuMass(units), m_e),
         uconvert(getuMass(units), m_n),
         uconvert(getuMass(units), m_p),
-        #uconvert(units[6] / units[4]^4 / units[2]^3, σ),
-        #uconvert(units[2]^-1, H),
+        uconvert(units[6] / units[4]^4 / units[2]^3, σ),
+        uconvert(units[2]^-1, H),
         uconvert(units[1]^2 * units[6] / units[2]^2 / units[4], k_B),
         uconvert(getuAcc(units), ACC0),
     )
@@ -97,13 +98,13 @@ function Constant(::Type{BigFloat}, units::Vector{Unitful.FreeUnits{N, D, nothin
     return Constant(
         uconvert(getuVel(units), BigFloat("299792458")u"m/s"),
         uconvert(units[1]^3 / units[6] / units[2]^2, BigFloat("6.67430e-11")u"m^3/kg/s^2"),
-        #uconvert(units[6] * units[1]^2 / units[2], BigFloat("6.62607015e-34")u"J*s"),
-        #uconvert(units[3] * units[2], BigFloat("1.602176634e-19")u"C"),
+        uconvert(units[6] * units[1]^2 / units[2], BigFloat("6.62607015e-34")u"J*s"),
+        uconvert(units[3] * units[2], BigFloat("1.602176634e-19")u"C"),
         uconvert(getuMass(units), BigFloat("9.1093837015e-31")u"kg"),
         uconvert(getuMass(units), BigFloat("1.67492749804e-27")u"kg"),
         uconvert(getuMass(units), BigFloat("1.67262192369e-27")u"kg"),
-        #uconvert(units[6] / units[4]^4 / units[2]^3, BigFloat("5.6703744191844294e-8")u"W/K^4/m^2"),
-        #uconvert(units[2]^-1, BigFloat("")),
+        uconvert(units[6] / units[4]^4 / units[2]^3, BigFloat("5.6703744191844294e-8")u"W/K^4/m^2"),
+        uconvert(units[2]^-1, BigFloat("2.399148907975663e-18")u"s^-1"),
         uconvert(units[1]^2 * units[6] / units[2]^2 / units[4], BigFloat("1.380649e-23")u"J/K"),
         uconvert(getuAcc(units), BigFloat("1.2e-8")u"cm/s^2"),
     )
@@ -117,13 +118,13 @@ function Constant(::Type{Measurement}, units::Vector{Unitful.FreeUnits{N, D, not
     return Constant(
         uconvert(getuVel(units), measurement(CODATA2018.c_0)),
         uconvert(units[1]^3 / units[6] / units[2]^2, measurement(CODATA2018.G)),
-        #uconvert(units[6] * units[1]^2 / units[2], measurement(CODATA2018.h)),
-        #uconvert(units[3] * units[2], measurement(CODATA2018.e)),
+        uconvert(units[6] * units[1]^2 / units[2], measurement(CODATA2018.h)),
+        uconvert(units[3] * units[2], measurement(CODATA2018.e)),
         uconvert(getuMass(units), measurement(CODATA2018.m_e)),
         uconvert(getuMass(units), measurement(CODATA2018.m_n)),
         uconvert(getuMass(units), measurement(CODATA2018.m_p)),
-        #uconvert(units[6] / units[4]^4 / units[2]^3, measurement(CODATA2018.σ)),
-        #uconvert(units[2]^-1, measurement(CODATA2018.H)),
+        uconvert(units[6] / units[4]^4 / units[2]^3, measurement(CODATA2018.σ)),
+        uconvert(units[2]^-1, measurement(HubbleConstant, 1.42/74.03*HubbleConstant)),
         uconvert(units[1]^2 * units[6] / units[2]^2 / units[4], measurement(CODATA2018.k_B)),
         uconvert(getuAcc(units), measurement(ACC0, 0.0*ACC0)),
     )
@@ -134,17 +135,18 @@ $(TYPEDSIGNATURES)
 """
 function Constant(::Type{T}, units::Vector{Unitful.FreeUnits{N, D, nothing} where D where N};
     ACC0 = ACC0,
+    H = HubbleConstant,
 ) where T<:Real
     return Constant(
         T(uconvert(getuVel(units), CODATA2018.c_0)),
         T(uconvert(units[1]^3 / units[6] / units[2]^2, CODATA2018.G)),
-        #T(uconvert(units[6] * units[1]^2 / units[2], CODATA2018.h)),
-        #T(uconvert(units[3] * units[2], CODATA2018.e)),
+        T(uconvert(units[6] * units[1]^2 / units[2], CODATA2018.h)),
+        T(uconvert(units[3] * units[2], CODATA2018.e)),
         T(uconvert(getuMass(units), CODATA2018.m_e)),
         T(uconvert(getuMass(units), CODATA2018.m_n)),
         T(uconvert(getuMass(units), CODATA2018.m_p)),
-        #T(uconvert(units[6] / units[4]^4 / units[2]^3, CODATA2018.σ)),
-        #T(uconvert(units[2]^-1, CODATA2018.H)),
+        T(uconvert(units[6] / units[4]^4 / units[2]^3, CODATA2018.σ)),
+        T(uconvert(units[2]^-1, H)),
         T(uconvert(units[1]^2 * units[6] / units[2]^2 / units[4], CODATA2018.k_B)),
         T(uconvert(getuAcc(units), ACC0)),
     )
@@ -156,13 +158,19 @@ end
 
 function Base.show(io::IO, c::Constant)
     print(io,
-        "Converted Constants:",
-        "\n    c = ", c.c,
-        "\n    G = ", c.G,
-        "\n    m_e = ", c.m_e,
-        "\n    m_n = ", c.m_n,
-        "\n    m_p = ", c.m_p,
-        "\n    k_B = ", c.k_B,
-        "\n    ACC0 = ", c.ACC0,
+        """
+        Converted Constants:
+               c = $(c.c) (light speed)
+               G = $(c.G) (Newtonian constant of gravitation)
+               h = $(c.h) (Planck constant)
+               e = $(c.e) (Elementary charge)
+             m_e = $(c.m_e) (Electron mass)
+             m_n = $(c.m_n) (Neutron mass)
+             m_p = $(c.m_p) (Protron mass)
+               σ = $(c.σ) (Stefan-Boltzmann constant)
+               H = $(c.H) (Hubble constant)
+             k_B = $(c.k_B) (Kelvin-Boltzmann constant)
+            ACC0 = $(c.ACC0) (Modified gravitational acceleration constant)
+        """
     )
 end
